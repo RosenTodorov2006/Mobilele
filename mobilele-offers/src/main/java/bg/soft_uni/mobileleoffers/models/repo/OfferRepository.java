c7c3c2e3bd3f56aecc2f50a8b54c6a51ee57f49a
Rosen Todorov <122826159+RosenTodorov2006@users.noreply.github.com>
2024-07-20 23:22:55 +0300

    Delete mobilele-offers directory

--- mobilele-offers/src/main/java/bg/soft_uni/mobileleoffers/models/repo/OfferRepository.java
+++
@@ -1,7 +0,0 @@
-package bg.soft_uni.mobileleoffers.models.repo;
-
-import bg.soft_uni.mobileleoffers.models.entity.Offer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OfferRepository extends JpaRepository<Offer, Long> {
-}
